927c4e398a7717fba8d9150fd732be2a4bdb3bf5
Csendes Kukac <csendes@kukacek.hu>
2026-02-02 16:18:45 +0100

    Konvertálás

---
+++ src/Konvert.java
@@ -0,0 +1,14 @@
+public class Konvert {
+    public static void csinal() {
+        String szoveg = "43";
+        int szam = Integer.parseInt(szoveg);
+        String szoveg2 = "43.12";
+        double szam2 = Double.parseDouble(szoveg2);
+        System.out.println(szam + " " + szam2);
+
+        int egesz = 35;
+        String egeszStr = String.valueOf(egesz);
+        
+    }
+
+}
